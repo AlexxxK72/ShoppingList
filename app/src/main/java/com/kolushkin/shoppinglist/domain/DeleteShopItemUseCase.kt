1ca5e06671b6b91aec001987e1c1ddf144e8208f
Alexandr Kolushkin <kolushkin@gmail.com>
2022-09-08 19:28:55 +0300

    Creating domain layer 1

---
+++ app/src/main/java/com/kolushkin/shoppinglist/domain/DeleteShopItemUseCase.kt
@@ -0,0 +1,7 @@
+package com.kolushkin.shoppinglist.domain
+
+class DeleteShopItemUseCase {
+    fun deleteShopItem(shopItem: ShopItem){
+
+    }
+}
---
+++ app/src/main/java/com/kolushkin/shoppinglist/domain/GetShopItemListUseCase.kt
@@ -0,0 +1,7 @@
+package com.kolushkin.shoppinglist.domain
+
+class GetShopItemListUseCase {
+    fun getShopItemList(): List<ShopItem>{
+        TODO()
+    }
+}
